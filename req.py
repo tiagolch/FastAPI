35ac03001a713994161d153879c3f1c021b7899b
Tiago Chaves <tiagodevch@gmail.com>
2022-04-15 01:06:04 -0300

    Acertado requisição e consulta
    Signed-off-by: Tiago Chaves <tiagodevch@gmail.com>

--- req.py
+++ req.py
@@ -1,6 +1,17 @@
 import requests
 
-retorno = requests.post('http://127.0.0.1:8000/usuario', params={"nome":"nao"})
+url = 'http://127.0.0.1:8000'
 
+parametro = {
+  "id": 3,
+  "nome": "Tiago",
+  "senha": "senha"
+}
+
+retorno = requests.post(f'{url}/usuario', json=parametro)
 print(retorno.json())
 
+
+lista = requests.get(f'{url}/listaUsuario')
+for i in lista:
+    print(i)
